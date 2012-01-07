6e53f5bf3f9b9da1c2b8af49a11378ac80e4776d
Alka Sharma <alka.pi.sharma@gmail.com>
2012-01-06 19:23:22 -0500

    Removing files

--- csc301_w11/dev/bookrequest.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	include_once('model.php');
-	$result = book_time($_SESSION['userinfo']["accountId"], $_POST["time"], $_POST["stuNum"],$_POST["courseCode"]); 
-?>
--- csc301_w11/dev/cancelrequest.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	include_once('model.php');
-	$result = cancel_time($_SESSION['userinfo']["accountId"], $_POST["time"]); 
-    echo $result;
-?>
